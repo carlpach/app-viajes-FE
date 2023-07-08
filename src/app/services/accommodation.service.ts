5303520882577cd6b2bf6aa7dec6c8cbdfdf47ce
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-07-08 18:56:23 +0200

    get accommodations by search

--- src/app/services/accommodation.service.ts
+++ src/app/services/accommodation.service.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment.local';
+import { AccommodationsI } from "../models/interfaces";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccommodationService {
 
+  private db_url = `${environment.db_url}/accommodation`;
   public placeholderImg: string =  "https://uning.es/wp-content/uploads/2016/08/ef3-placeholder-image.jpg";
+
+  accommodSearched!: AccommodationsI[];
 
 
   constructor(private http: HttpClient) { }
 
   public getAccommodations() {
-    return this.http.get(environment.db_url); 
+    return this.http.get(this.db_url); 
   }   
 
-  public getAccommodationsBySearch(city: String, checkin: Date, checkout: Date, adults: String) {
-    return this.http.get(`${environment.db_url}/?city=${city}&checkin=${checkin}&checkout=${checkout}&adults=${adults}`);
+  public getAccommodationsBySearch(city: String, checkin: String, checkout: String, adults: String) {
+    return this.http.get(`${this.db_url}/search?city=${city}&checkin=${checkin}&checkout=${checkout}&adults=${adults}`);
   }   
 
+
+  public setAccommod(accommodSearched: any){
+    this.accommodSearched = accommodSearched;
+  }
 
 
 }
